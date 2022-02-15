2ee7abae03e181e69a44c5a664492b5144c3c3a3
AndrewCarlson1996 <70498953+AndrewCarlson1996@users.noreply.github.com>
2022-02-15 10:47:46 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 echo Building...
 
 sudo apt install npm
-npm install eslint
-npm install htmlinter
+npm install eslint --save-dev
+npx eslint --init
+npm i htmlint@0.0.3
 
-eslint *.js
-htmlhint *.html
+eslint src/*.js
+node node_modules/htmlint src/*.html
 
 zip -r EMA.zip src
 
