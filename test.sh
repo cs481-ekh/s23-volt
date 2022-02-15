5e355ba34cfe7c4b4c81182ce2e7d3e56c5ea7e2
AndrewCarlson1996 <70498953+AndrewCarlson1996@users.noreply.github.com>
2022-02-15 11:15:13 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,7 @@
 echo Testing...
 
 sudo apt install unzip
-mkdir Test_Directory
-sudo unzip EMA.zip -d /Test_Directory
-diff -q src/ Test_Directory/
+unzip EMA.zip -d /test_directory
+diff -q src/ test_directory/
 
 exit 0
